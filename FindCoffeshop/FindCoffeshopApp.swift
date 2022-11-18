af7c72eac4d4058947ef61ba921c667e819f7f21
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-11-18 23:35:20 +0700

    Initial Commit

---
+++ FindCoffeshop/FindCoffeshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindCoffeshopApp.swift
+//  FindCoffeshop
+//
+//  Created by Ahmad Syarifuddin Randiko on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FindCoffeshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
